3f443a17b26ac5c158c58f3876ff1ad5506a170d
AgamzBot <95152189+AgamzBot@users.noreply.github.com>
2021-12-01 14:20:44 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -1,23 +1,35 @@
 const pc_sewa = (_minggu, __minggu, ___minggu, _bulan, _permanen, __permanen) => {
 return `「 *PRICE LIST* 」
 							
-⬣ PRICE SEWABOT
+``` [ JASA SEWA BOT 🤖 ]
 
-⬡ 1 MINGGU = ${_minggu}
-⬡ 2 MINGGU = ${__minggu}
-⬡ 3 MINGGU = ${___minggu}
-⬡ 1 BULAN = ${_bulan}
-⬡ PERMANEN = ${_permanen}
-⬡ PERMANEN + USER PREM = ${__permanen}
-
-
-⬣ PRICE JADIBOT
-
-⬡ JADI BOT PERMANEN 25K
-
-
-⬣ MINAT? PM
-⬡ wa.me/6282127644092 
+┏━━〔 HARGA PERMANEN 〕━⬣
+┃⬡ 5K - 1GRUP √
+┃⬡ 8K - 2GRUP √
+┃⬡ 10K - 4RUP √
+┗━━━━━━━━⬣
+┏━━〔 FITUR BOT 〕━⬣
+┃⬡ ANTILINK GRUP 
+┃⬡ WELCOME SIMPEL
+┃⬡ ON 24 JAM
+┃⬡ STICKER 
+┃⬡ ATTP
+┃⬡ FITUR BANYAK
+┃⬡ 18+
+┃⬡ DLL
+┗━━━━━━━━⬣
+┏━━〔 PAYMENT 〕━⬣
+┃⬡ GOPAY
+┃⬡ DANA
+┃⬡ PULSA + 2K
+┗━━━━━━━━⬣
+┏━━〔 MINAT HUB OWNER 〕━⬣
+┃⬡ https://wa.me/6282127644092?text=Bang+Mau+Sewa+Bot
+┃
+┃⬡» _Bisa Test Bot Terlebih Dahulu_ 
+~*JIKA MAU TEST BOT MASUK  SINI*~
+https://chat.whatsapp.com/JzBGYQz04we9371guBkAGM
+┗━━━━━━━━⬣```
 
 
 NOTE : 
